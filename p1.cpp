f246e5e5fcd28c0489a95842b69e189c30112ff0
Andreia <andreia.sofia.pereira@tecnico.ulisboa.pt>
2019-03-16 18:01:32 +0000

    finished

--- p1.cpp
+++ p1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include<stack>
+#include <stack>
+#include <algorithm>
 
 #define UNVISITED -1
 #define NIL -1
@@ -18,6 +19,7 @@ private:
     int _n_networks;
     vector<int> _networks;
     vector<int> _aps;
+    int _biggest_scc, _preemptive_biggest = 0;
 public:
     Graph(int n){
         _n_nodes = n;
@@ -29,13 +31,16 @@ public:
         _matrix.at(v2).push_back(v1);
     }
 
-    void visit(int* d, int* low, bool* on_stack, stack<int> *visited, int* parent, int pos);
+    void visit(int* d, int* low, bool* on_stack, stack<int> *visited, int* parent,  bool *is_ap, int pos);
+
+    void find_biggest_scc(int* d, int* low, bool* on_stack, stack<int> *visited, bool *is_ap, int pos);
 
     void tarjan_scc(){
         int *d = new int[_n_nodes];
         int *low = new int[_n_nodes];
         int *parent = new int[_n_nodes];
         bool *on_stack = new bool[_n_nodes];
+        bool *is_ap = new bool[_n_nodes];
         stack<int> *visited = new stack<int>();
 
         for(int i =0; i< _n_nodes; i++){
@@ -43,56 +48,68 @@ public:
             low[i] = UNVISITED;
             parent[i] = NIL;
             on_stack[i] = false;
+            is_ap[i] = false;
         }
         for(int i=0; i<_n_nodes; i++){
             if(d[i] == UNVISITED){
-                visit(d, low, on_stack, visited, parent, i);
+                visit(d, low, on_stack, visited, parent,is_ap, i);
             }
         }
+        if(_aps.size() != 0){
+            for(int i =0; i< _n_nodes; i++){
+                d[i]=UNVISITED;
+                low[i] = UNVISITED;
+                on_stack[i] = false;
+            } 
+            d_time = 0;
+            for(int i=0; i<_n_nodes; i++){
+                if(d[i] == UNVISITED){
+                    find_biggest_scc(d, low, on_stack, visited,is_ap, i);
+                }
+            }
+        }
+        else{
+            _biggest_scc = _preemptive_biggest;
+        }
+
     }
 
     /* DEBUG START */
     void print_graph(){
-        cout << _n_nodes << endl;
-        for(int i =0; i< _n_nodes; i++){
-            cout << i +1<< " : ";
-            for (int j=0; j< _matrix.at(i).size(); j++){
-                cout << _matrix.at(i).at(j) +1 << "  ";
-            }
-            cout << endl;
+        printf("%d\n", _n_networks);
+        sort(_networks.begin(), _networks.end());
+        unsigned int i;
+        for(i =0; i<_networks.size() - 1; i++){
+            printf("%d ", _networks.at(i) + 1);
         }
-        cout <<endl; 
-        cout<<"Networks: "<<endl;
-        for(int i =0; i<_networks.size(); i++){
-            cout << _networks.at(i)  + 1<< endl;
-        }
-        cout << "Aps: "<< endl;
-        for(int i =0; i<_aps.size(); i++){
-                cout << _aps.at(i) + 1<< endl;
-            }
+        printf("%d", _networks.at(i) + 1);
+        printf("\n%lu\n", _aps.size());
+        printf("%d\n", _biggest_scc);
     }
 
     /* DEBUG END */
 };
 
-void Graph::visit(int* d, int* low, bool* on_stack, stack<int> *visited, int *parent, int pos){
+void Graph::visit(int* d, int* low, bool* on_stack, stack<int> *visited, int *parent,  bool *is_ap, int pos){
     int children = 0;
     d[pos] = low[pos] = d_time++;
     visited->push(pos);
     on_stack[pos] = true;
-    for(int i = 0; i< _matrix.at(pos).size(); i++){
+    for(unsigned int i = 0; i< _matrix.at(pos).size(); i++){
         int adj = _matrix.at(pos).at(i);
         if(d[adj] == UNVISITED){
             children++;
             parent[adj] = pos;
-            visit(d, low, on_stack, visited, parent,adj);
+            visit(d, low, on_stack, visited, parent, is_ap, adj);
             low[pos] = MIN(low[pos], low[adj]);
 
-            if(parent[pos] == NIL && children >=2){
+            if(parent[pos] == NIL && children >=2 && !is_ap[pos]){
                 _aps.push_back(pos);
+                is_ap[pos] = true;
             }
-            else if (parent[pos] != NIL && low[adj] >= d[pos]){
+            else if (parent[pos] != NIL && low[adj] >= d[pos] && !is_ap[pos]){
                 _aps.push_back(pos);
+                is_ap[pos] = true;
             }
         }
         else if(on_stack[adj]){
@@ -102,16 +119,51 @@ void Graph::visit(int* d, int* low, bool* on_stack, stack<int> *visited, int *pa
     int curr;
     if(low[pos] == d[pos]){
         int max = NIL;
+        int size = 0;
         while(visited->top() != pos){
             curr = visited->top();
             on_stack[curr] = false;
             if(curr > max){max = curr;}
             visited->pop();
+            size++;
         }
         on_stack[pos] = false;
+        size++;
         if(pos>max){max=pos;}
+        if(size > _preemptive_biggest){ _preemptive_biggest = size;}
         visited->pop();
         _networks.push_back(max);
+        _n_networks++;
+    }
+}
+
+void Graph::find_biggest_scc(int* d, int* low, bool* on_stack, stack<int> *visited,  bool *is_ap, int pos){
+    d[pos] = low[pos] = d_time++;
+    visited->push(pos);
+    on_stack[pos] = true;
+    for(unsigned int i = 0; i< _matrix.at(pos).size(); i++){
+        int adj = _matrix.at(pos).at(i);
+        if(d[adj] == UNVISITED && !is_ap[adj]){
+            find_biggest_scc(d, low, on_stack, visited, is_ap, adj);
+            low[pos] = MIN(low[pos], low[adj]);
+        }
+        else if(on_stack[adj] && !is_ap[adj]){
+            low[pos] = MIN(low[pos], d[adj]);
+        }
+    }
+    int size=0;
+    int curr;
+    if(low[pos] == d[pos]){
+        while(visited->top() != pos){
+            curr = visited->top();
+            on_stack[curr] = false;
+            visited->pop();
+            size++;
+        }
+        on_stack[pos] = false;
+        visited->pop();
+        size++;
+        if(size > _biggest_scc){ _biggest_scc = size;}
     }
 }
 
